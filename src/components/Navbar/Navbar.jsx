c46ad248e31da1164a9cf92a0ebac94ebfb5322c
Nicolas <nicopazmalizia@gmail.com>
2024-08-14 22:58:52 -0300

    Finalizo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import DarkMode from "./DarkMode";
 
@@ -25,6 +25,24 @@ const MenuLinks = [
     link: "/#blogs",
   },
 ];
+
+const DropdownLinks = [
+  { 
+    id: 1, 
+    name: "Trending Products", 
+    link: "/#",
+  },
+  { 
+    id: 2, 
+    name: "Best Selling", 
+    link: "/#", 
+  },
+  { 
+    id: 3,
+     name: "Top Rated", 
+     link: "/#", 
+    },
+]
 
 const Navbar = () => {
   return (
@@ -52,6 +70,33 @@ const Navbar = () => {
                     </a>
                   </li>
                 ))}
+                {/* Dropdown */}
+                <li className="relative cursor-pointer group">
+                  <a
+                    href="#"
+                    className="flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2"
+                  >
+                    Quick Links
+                    <span>
+                      <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                    </span>
+                  </a>
+                  {/* Dropdown Links */}
+                  <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2  dark:text-white ">
+                    <ul className="space-y-2">
+                      {
+                        DropdownLinks.map((data,index) => (
+                          <li>
+                            <a className="text-gray-500 hover:text-black dark:hover:text-white duration-200 inline-block w-full p-2 hover:bg-primary/20 rounded-md font-semibold" 
+                            href={data.link}>
+                              {data.name}
+                            </a>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
@@ -64,15 +109,15 @@ const Navbar = () => {
               <IoSearch className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2  right-3 duration-200" />
             </div>
             {/* Order-button section */}
-              <button className="relative p-3">
-                <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400"/>
-                <div
-                className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs"
-                >4</div>
-              </button>
+            <button className="relative p-3">
+              <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400" />
+              <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">
+                4
+              </div>
+            </button>
             {/* Dark mode section */}
             <div>
-              <DarkMode/>
+              <DarkMode />
             </div>
           </div>
         </div>
